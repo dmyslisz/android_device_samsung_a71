e85effcb3e602c6b7d1117775f90ce365428fd64
dmyslisz <157851922+dmyslisz@users.noreply.github.com>
2026-03-20 17:00:17 +0100

    Update TARGET_BOARD_NAME from SRPSF18B010 to SRPSF18B011

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,11 +12,11 @@ DEVICE_PATH := device/samsung/a71
 TARGET_OTA_ASSERT_DEVICE := a71
 
 # Board
-TARGET_BOARD_NAME := SRPSF18B010
+TARGET_BOARD_NAME := SRPSF18B011
 
 # FOD
 $(call soong_config_set,samsung_udfps,udfps_zorder,0x20000000u)
-TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/samsung/fingerprint:libudfps_extension.samsung
+
 TARGET_USES_FOD_ZPOS := true
 
 # Kernel
